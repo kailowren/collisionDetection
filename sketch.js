6b2472ac76049d12c4adf08ffbc439b3b9278a33
kailowren <71066878+kailowren@users.noreply.github.com>
2020-09-14 17:58:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var movingRect,fixedRect;
+
+function setup() {
+
+  createCanvas(800,400);
+
+ fixedRect=createSprite(400,200,50,80);
+ movingRect= createSprite(400, 300, 80, 30);
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+ movingRect.y=World.mouseY;
+ movingRect.x=World.mouseX;
+
+ if( movingRect.x - fixedRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+    fixedRect.x - movingRect.x <= movingRect.width/2 + movingRect.width/2 &&
+     movingRect.y - fixedRect.y <= movingRect.height/2 + movingRect.height/2&&
+     fixedRect.y - movingRect.y <= movingRect.height/2 + movingRect.height/2
+    ){
+   movingRect.shapeColor="red";
+   fixedRect.shapeColor="red";
+ }
+ else {
+  movingRect.shapeColor="green";
+  fixedRect.shapeColor="green"; 
+ }
+  
+ 
+ 
+ 
+ 
+ drawSprites();
+}
